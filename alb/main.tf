fc2b866a834f446546a7815b1884a5d0cac7f4d2
by-sabbir <me@sabbir.dev>
2022-11-12 17:09:59 +0600

    refactor: port changed to 80;

--- alb/main.tf
+++ alb/main.tf
@@ -36,7 +36,7 @@ resource "aws_lb" "alb" {
 
 resource "aws_lb_target_group" "ecs" {
   name     = "ecs"
-  port     = 8888
+  port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "ip"
